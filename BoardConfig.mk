549fb214b6aac83c48ad3e5a18492f3920015714
莫名其妙2333 <rdgbnirr3418435@163.com>
2025-05-14 15:29:25 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -101,7 +101,7 @@ BOARD_USES_QCOM_HARDWARE := true
 TW_INCLUDE_CRYPTO := true
 
 # OrangeFox Recovery
-OF_FL_PATH1 := /proc/qcom_flash
+OF_FL_PATH1 := "/proc/qcom_flash"
 
 # exFAT FS Support
 TW_INCLUDE_FUSE_EXFAT := true
